d61e96f0386b789875c0c48b4bd9722f2e4b2742
yehs <961414964@qq.com>
2022-01-04 21:25:33 +0800

    前后端整合

---
+++ predict.py
@@ -0,0 +1,57 @@
+SZ = 20  # 训练图片长宽
+MAX_WIDTH = 1000  # 原始图片最大宽度
+Min_Area = 2000  # 车牌区域允许最大面积
+PROVINCE_START = 1000
+
+
+# 读取图片文件
+def imreadex(filename):
+    return cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+
+def point_limit(point):
+    if point[0] < 0:
+        point[0] = 0
+    if point[1] < 0:
+        point[1] = 0
+
+
+# 根据设定的阈值和图片直方图，找出波峰，用于分隔字符
+def find_waves(threshold, histogram):
+    up_point = -1  # 上升点
+    is_peak = False
+    if histogram[0] > threshold:
+        up_point = 0
+        is_peak = True
+    wave_peaks = []
+    for i, x in enumerate(histogram):
+        if is_peak and x < threshold:
+            if i - up_point > 2:
+                is_peak = False
+                wave_peaks.append((up_point, i))
+        elif not is_peak and x >= threshold:
+            is_peak = True
+            up_point = i
+    if is_peak and up_point != -1 and i - up_point > 4:
+        wave_peaks.append((up_point, i))
+    return wave_peaks
+
+
+# 根据找出的波峰，分隔图片，从而得到逐个字符图片
+def seperate_card(img, waves):
+    part_cards = []
+    for wave in waves:
+        part_cards.append(img[:, wave[0]:wave[1]])
+    return part_cards
+
+
+# 来自opencv的sample，用于svm训练
+def deskew(img):
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11'] / m['mu02']
+    M = np.float32([[1, skew, -0.5 * SZ * skew], [0, 1, 0]])
+    img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+    return img
+# 来自opencv的sample，用于svm训练
